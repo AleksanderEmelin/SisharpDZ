33e3a12718d54ee7e2a9e70494692111feb16e78
Aleksabder <emelin.ss@mail.ru>
2023-07-12 12:06:59 +0300

    Task 2 Task 4

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿void Task2()
+{
+Console.WriteLine("Введите первое число:");
+int number_a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+int number_b = Convert.ToInt32(Console.ReadLine());
+
+if (number_a < number_b)
+{
+     Console.WriteLine("Второе число больше первого");
+}
+else
+{
+     Console.WriteLine("первое число больше второго");
+}
+}
+
+
+void Task4()
+{
+ Console.WriteLine("Введите первое число:");
+ int number_a = Convert.ToInt32(Console.ReadLine());
+ Console.WriteLine("Введите второе число:");
+ int number_b = Convert.ToInt32(Console.ReadLine());  
+ Console.WriteLine("Введите третье число:");
+ int number_c = Convert.ToInt32(Console.ReadLine());
+
+ int max = number_a;
+
+ if (number_a > max) max = number_a;
+
+ if (number_b > max) max = number_b;
+       
+ if (number_c > max) max = number_c;
+                       
+ Console.Write("max = ");
+ Console.WriteLine(max);
+        
+}
